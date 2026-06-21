e25f5ccd6eed09346445243acb943d142494dab6
RAMBOO <ramboooo@126.com>
2026-06-22 05:14:41 +0800

    fix: API认证 - 支持token签名和BasicAuth头
    - 从customRequest.js中逆向出MD5 token签名算法
      (secret=c5c3f201a8e8fc634d37a766a0299218)
    - 修复Authorization头格式: Basic -> BasicAuth
    - 添加内联MD5实现用于生成token
    - 自动从GET响应合并Set-Cookie会话
    - 添加价格数据成功获取的调试日志

--- jd-price-history.user.js
+++ jd-price-history.user.js
@@ -11,8 +11,9 @@
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_deleteValue
+// @grant        GM_registerMenuCommand
 // @grant        GM_openInTab
-// @require      https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/echarts/5.3.0-rc.1/echarts.common.min.js
+// @require      https://cdn.jsdelivr.net/npm/echarts@5.3.0/dist/echarts.min.js
 // @connect      manmanbuy.com
 // @license      MIT
 // ==/UserScript==
@@ -288,6 +289,134 @@
     }
 
     /* ============================================================
+     *  MD5（由 js 实现，用于签名）
+     * ============================================================ */
+    var md5 = (function () {
+        var rotateLeft = function (lValue, iShiftBits) { return (lValue << iShiftBits) | (lValue >>> (32 - iShiftBits)); };
+        var addUnsigned = function (lX, lY) {
+            var lX4, lY4, lX8, lY8, lResult;
+            lX8 = (lX & 0x80000000); lY8 = (lY & 0x80000000);
+            lX4 = (lX & 0x40000000); lY4 = (lY & 0x40000000);
+            lResult = (lX & 0x3FFFFFFF) + (lY & 0x3FFFFFFF);
+            if (lX4 & lY4) return (lResult ^ 0x80000000 ^ lX8 ^ lY8);
+            if (lX4 | lY4) {
+                if (lResult & 0x40000000) return (lResult ^ 0xC0000000 ^ lX8 ^ lY8);
+                else return (lResult ^ 0x40000000 ^ lX8 ^ lY8);
+            } else return (lResult ^ lX8 ^ lY8);
+        };
+        var _F = function (x, y, z) { return (x & y) | ((~x) & z); };
+        var _G = function (x, y, z) { return (x & z) | (y & (~z)); };
+        var _H = function (x, y, z) { return x ^ y ^ z; };
+        var _I = function (x, y, z) { return y ^ (x | (~z)); };
+        var _FF = function (a, b, c, d, x, s, ac) {
+            a = addUnsigned(a, addUnsigned(addUnsigned(_F(b, c, d), x), ac));
+            return addUnsigned(rotateLeft(a, s), b);
+        };
+        var _GG = function (a, b, c, d, x, s, ac) {
+            a = addUnsigned(a, addUnsigned(addUnsigned(_G(b, c, d), x), ac));
+            return addUnsigned(rotateLeft(a, s), b);
+        };
+        var _HH = function (a, b, c, d, x, s, ac) {
+            a = addUnsigned(a, addUnsigned(addUnsigned(_H(b, c, d), x), ac));
+            return addUnsigned(rotateLeft(a, s), b);
+        };
+        var _II = function (a, b, c, d, x, s, ac) {
+            a = addUnsigned(a, addUnsigned(addUnsigned(_I(b, c, d), x), ac));
+            return addUnsigned(rotateLeft(a, s), b);
+        };
+        var convertToWordArray = function (str) {
+            var lWordCount, lMessageLength = str.length, lNumberOfWords_temp1 = lMessageLength + 8,
+                lNumberOfWords_temp2 = (lNumberOfWords_temp1 - (lNumberOfWords_temp1 % 64)) / 64,
+                lNumberOfWords = (lNumberOfWords_temp2 + 1) * 16, lWordArray = Array(lNumberOfWords - 1),
+                lBytePosition = 0, lByteCount = 0;
+            while (lByteCount < lMessageLength) {
+                lWordCount = (lByteCount - (lByteCount % 4)) / 4;
+                lBytePosition = (lByteCount % 4) * 8;
+                lWordArray[lWordCount] = (lWordArray[lWordCount] | (str.charCodeAt(lByteCount) << lBytePosition));
+                lByteCount++;
+            }
+            lWordCount = (lByteCount - (lByteCount % 4)) / 4;
+            lBytePosition = (lByteCount % 4) * 8;
+            lWordArray[lWordCount] = lWordArray[lWordCount] | (0x80 << lBytePosition);
+            lWordArray[lNumberOfWords - 2] = lMessageLength << 3;
+            lWordArray[lNumberOfWords - 1] = lMessageLength >>> 29;
+            return lWordArray;
+        };
+        var wordToHex = function (lValue) {
+            var WordToHexValue = '', WordToHexValue_temp = '', lByte, lCount;
+            for (lCount = 0; lCount <= 3; lCount++) {
+                lByte = (lValue >>> (lCount * 8)) & 255;
+                WordToHexValue_temp = '0' + lByte.toString(16);
+                WordToHexValue += WordToHexValue_temp.substr(WordToHexValue_temp.length - 2, 2);
+            }
+            return WordToHexValue;
+        };
+        var u = function (k) {
+            var x = convertToWordArray(k), a = 0x67452301, b = 0xEFCDAB89, c = 0x98BADCFE, d = 0x10325476,
+                S11 = 7, S12 = 12, S13 = 17, S14 = 22, S21 = 5, S22 = 9, S23 = 14, S24 = 20, S31 = 4, S32 = 11, S33 = 16, S34 = 23,
+                S41 = 6, S42 = 10, S43 = 15, S44 = 21;
+            for (var kk = 0; kk < x.length; kk += 16) {
+                var AA = a, BB = b, CC = c, DD = d;
+                a = _FF(a, b, c, d, x[kk + 0], S11, 0xD76AA478); d = _FF(d, a, b, c, x[kk + 1], S12, 0xE8C7B756);
+                c = _FF(c, d, a, b, x[kk + 2], S13, 0x242070DB); b = _FF(b, c, d, a, x[kk + 3], S14, 0xC1BDCEEE);
+                a = _FF(a, b, c, d, x[kk + 4], S11, 0xF57C0FAF); d = _FF(d, a, b, c, x[kk + 5], S12, 0x4787C62A);
+                c = _FF(c, d, a, b, x[kk + 6], S13, 0xA8304613); b = _FF(b, c, d, a, x[kk + 7], S14, 0xFD469501);
+                a = _FF(a, b, c, d, x[kk + 8], S11, 0x698098D8); d = _FF(d, a, b, c, x[kk + 9], S12, 0x8B44F7AF);
+                c = _FF(c, d, a, b, x[kk + 10], S13, 0xFFFF5BB1); b = _FF(b, c, d, a, x[kk + 11], S14, 0x895CD7BE);
+                a = _FF(a, b, c, d, x[kk + 12], S11, 0x6B901122); d = _FF(d, a, b, c, x[kk + 13], S12, 0xFD987193);
+                c = _FF(c, d, a, b, x[kk + 14], S13, 0xA679438E); b = _FF(b, c, d, a, x[kk + 15], S14, 0x49B40821);
+                a = _GG(a, b, c, d, x[kk + 1], S21, 0xF61E2562); d = _GG(d, a, b, c, x[kk + 6], S22, 0xC040B340);
+                c = _GG(c, d, a, b, x[kk + 11], S23, 0x265E5A51); b = _GG(b, c, d, a, x[kk + 0], S24, 0xE9B6C7AA);
+                a = _GG(a, b, c, d, x[kk + 5], S21, 0xD62F105D); d = _GG(d, a, b, c, x[kk + 10], S22, 0x2441453);
+                c = _GG(c, d, a, b, x[kk + 15], S23, 0xD8A1E681); b = _GG(b, c, d, a, x[kk + 4], S24, 0xE7D3FBC8);
+                a = _GG(a, b, c, d, x[kk + 9], S21, 0x21E1CDE6); d = _GG(d, a, b, c, x[kk + 14], S22, 0xC33707D6);
+                c = _GG(c, d, a, b, x[kk + 3], S23, 0xF4D50D87); b = _GG(b, c, d, a, x[kk + 8], S24, 0x455A14ED);
+                a = _GG(a, b, c, d, x[kk + 13], S21, 0xA9E3E905); d = _GG(d, a, b, c, x[kk + 2], S22, 0xFCEFA3F8);
+                c = _GG(c, d, a, b, x[kk + 7], S23, 0x676F02D9); b = _GG(b, c, d, a, x[kk + 12], S24, 0x8D2A4C8A);
+                a = _HH(a, b, c, d, x[kk + 5], S31, 0xFFFA3942); d = _HH(d, a, b, c, x[kk + 8], S32, 0x8771F681);
+                c = _HH(c, d, a, b, x[kk + 11], S33, 0x6D9D6122); b = _HH(b, c, d, a, x[kk + 14], S34, 0xFDE5380C);
+                a = _HH(a, b, c, d, x[kk + 1], S31, 0xA4BEEA44); d = _HH(d, a, b, c, x[kk + 4], S32, 0x4BDECFA9);
+                c = _HH(c, d, a, b, x[kk + 7], S33, 0xF6BB4B60); b = _HH(b, c, d, a, x[kk + 10], S34, 0xBEBFBC70);
+                a = _HH(a, b, c, d, x[kk + 13], S31, 0x289B7EC6); d = _HH(d, a, b, c, x[kk + 0], S32, 0xEAA127FA);
+                c = _HH(c, d, a, b, x[kk + 3], S33, 0xD4EF3085); b = _HH(b, c, d, a, x[kk + 6], S34, 0x4881D05);
+                a = _HH(a, b, c, d, x[kk + 9], S31, 0xD9D4D039); d = _HH(d, a, b, c, x[kk + 12], S32, 0xE6DB99E5);
+                c = _HH(c, d, a, b, x[kk + 15], S33, 0x1FA27CF8); b = _HH(b, c, d, a, x[kk + 2], S34, 0xC4AC5665);
+                a = _II(a, b, c, d, x[kk + 0], S41, 0xF4292244); d = _II(d, a, b, c, x[kk + 7], S42, 0x432AFF97);
+                c = _II(c, d, a, b, x[kk + 14], S43, 0xAB9423A7); b = _II(b, c, d, a, x[kk + 5], S44, 0xFC93A039);
+                a = _II(a, b, c, d, x[kk + 12], S41, 0x655B59C3); d = _II(d, a, b, c, x[kk + 3], S42, 0x8F0CCC92);
+                c = _II(c, d, a, b, x[kk + 10], S43, 0xFFEFF47D); b = _II(b, c, d, a, x[kk + 1], S44, 0x85845DD1);
+                a = _II(a, b, c, d, x[kk + 8], S41, 0x6FA87E4F); d = _II(d, a, b, c, x[kk + 15], S42, 0xFE2CE6E0);
+                c = _II(c, d, a, b, x[kk + 6], S43, 0xA3014314); b = _II(b, c, d, a, x[kk + 13], S44, 0x4E0811A1);
+                a = _II(a, b, c, d, x[kk + 4], S41, 0xF7537E82); d = _II(d, a, b, c, x[kk + 11], S42, 0xBD3AF235);
+                c = _II(c, d, a, b, x[kk + 2], S43, 0x2AD7D2BB); b = _II(b, c, d, a, x[kk + 9], S44, 0xEB86D391);
+                a = addUnsigned(a, AA); b = addUnsigned(b, BB); c = addUnsigned(c, CC); d = addUnsigned(d, DD);
+            }
+            return (wordToHex(a) + wordToHex(b) + wordToHex(c) + wordToHex(d)).toUpperCase();
+        };
+        return u;
+    })();
+
+    var MMB_SECRET = 'c5c3f201a8e8fc634d37a766a0299218';
+
+    function generateToken(params) {
+        var p = {};
+        for (var k in params) p[k] = params[k];
+        p.t = Date.now();
+        var keys = Object.keys(p).sort();
+        var str = MMB_SECRET;
+        for (var i = 0; i < keys.length; i++) {
+            var key = keys[i];
+            if (p[key] != null && p[key] !== '') {
+                str += encodeURIComponent(key) + encodeURIComponent(p[key]);
+            }
+        }
+        str += MMB_SECRET;
+        str = str.toUpperCase();
+        p.token = md5(str);
+        return p;
+    }
+
+    /* ============================================================
      *  Cookie 管理
      * ============================================================ */
     function getCookie() { return GM_getValue(MMB_COOKIE_KEY, ''); }
@@ -345,12 +474,12 @@
             settingsEl.querySelector('#jd-ph-cookie-input').value = existing;
         }
         settingsEl.classList.add('open');
-        overlay.classList.add('active');
+        if (overlay) overlay.classList.add('active');
     }
 
     function hideSettings() {
         if (settingsEl) settingsEl.classList.remove('open');
-        overlay.classList.remove('active');
+        if (overlay) overlay.classList.remove('active');
     }
 
     function showSettingsStatus(type, msg) {
@@ -538,30 +667,55 @@
             onload: function(pageRes) {
                 if (pageRes.status !== 200) { showError('页面加载失败', true); return; }
                 var html = pageRes.responseText;
+                // 捕获 Set-Cookie，合并到 cookie 中
+                if (pageRes.responseHeaders) {
+                    var setCookieMatch = pageRes.responseHeaders.match(/set-cookie[^:]*:\s*([^\r\n]+)/i);
+                    if (setCookieMatch) {
+                        var extraCookies = setCookieMatch[1].split(';')[0];
+                        if (cookie.indexOf(extraCookies.split('=')[0]) === -1) {
+                            cookie = (cookie ? cookie + '; ' : '') + extraCookies;
+                            console.log('[jd-ph] 合并了新Cookie:', extraCookies.split('=')[0]);
+                        }
+                    }
+                }
                 // 提取 ticket
                 var m = html.match(/id="ticket" value="([^"]+)"/);
                 if (!m) { showError('无法获取票据，请确认Cookie是否有效', true); return; }
                 var ticket = m[1];
+                console.log('[jd-ph] 原始ticket:', ticket);
                 if (ticket.length > 4) ticket = ticket.substr(ticket.length - 4, 4) + ticket.substr(0, ticket.length - 4);
+                console.log('[jd-ph] 转换后ticket:', ticket);
 
                 // 调 API
+                var apiParams = generateToken({ method: 'getHistoryTrend', key: jdUrl });
+                var postData = 'method=' + encodeURIComponent(apiParams.method) + '&key=' + encodeURIComponent(apiParams.key) + '&t=' + apiParams.t + '&token=' + apiParams.token;
+                console.log('[jd-ph] POST数据: t=' + apiParams.t + ' token=' + apiParams.token);
                 GM_xmlhttpRequest({
                     method: 'POST',
                     url: 'https://tool.manmanbuy.com/api.ashx',
-                    data: 'method=getHistoryTrend&key=' + encodeURIComponent(jdUrl),
+                    data: postData,
                     headers: {
                         'Cookie': cookie,
-                        'Authorization': 'Basic ' + ticket,
+                        'Authorization': 'BasicAuth ' + ticket,
                         'Content-Type': 'application/x-www-form-urlencoded',
                         'User-Agent': navigator.userAgent,
+                        'X-Requested-With': 'XMLHttpRequest',
+                        'Referer': 'https://tool.manmanbuy.com/HistoryLowest.aspx?url=' + encodeURIComponent(jdUrl),
                     },
                     onload: function(apiRes) {
                         try {
                             var ret = JSON.parse(apiRes.responseText);
                             if (ret.code !== 0 || !ret.data || !ret.data.datePrice) {
-                                showError(ret.msg || '暂无价格数据', true);
+                                console.log('[jd-ph] API 响应:', apiRes.responseText);
+                                console.log('[jd-ph] ticket:', ticket);
+                                showError((ret.msg || '暂无价格数据') + ' (code=' + ret.code + ')', true);
                                 return;
                             }
+                            console.log('[jd-ph] 价格数据获取成功!');
+                            console.log('[jd-ph] 商品:', ret.data.spName);
+                            console.log('[jd-ph] 当前价:', ret.data.currentPrice);
+                            console.log('[jd-ph] 历史最低:', ret.data.lowerPrice, ret.data.lowerDate);
+                            console.log('[jd-ph] 价格点数:', ret.data.datePrice.split('],[').length);
                             // 解析 datePrice: "[ts1,p1,''],[ts2,p2,'']"
                             var rawData = eval('([' + ret.data.datePrice + '])');
                             var categories = rawData.map(function(d) { return new Date(d[0]); });
@@ -591,7 +745,9 @@
      *  初始化
      * ============================================================ */
     function init() {
-        // 确认是商品详情页（包含数字ID + .html）
+        GM_registerMenuCommand('⚙ 设置慢慢买 Cookie', function() {
+            showSettings();
+        });
         if (!/item\.jd\.com\/\d+\.html/i.test(location.href)) return;
         buildUI();
     }
